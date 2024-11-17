c9b04142c19712da86ef407d9168016acbef4c87
Tubagus Farhan Adnan <3337220038@untirta.ac.id>
2024-11-17 17:41:04 +0700

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+# routes.py
+from flask import Blueprint
+
+
+
+user_routes = Blueprint('user_routes', __name__)
+
+# user_routes.route('/users', methods=['GET'])(get_all_users)
+# user_routes.route('/users/<string:user_uuid>', methods=['PUT'])(update_user_by_uuid)
+# user_routes.route('/users/<string:user_uuid>', methods=['GET'])(get_user_by_uuid)
+# user_routes.route('/users/<string:user_uuid>', methods=['DELETE'])(delete_user_by_uuid)
+
+# user_routes.route('/regis', methods=['POST'])(regis)
+
+# user_routes.route('/login', methods=['POST'])(login)
+
+# user_routes.route('/admin/users/delete', methods=['DELETE'])(delete_user_all)
+
